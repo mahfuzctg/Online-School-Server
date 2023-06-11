8799b6ef5eaf66a0830403d17a803cec72b82605
Mahfuz <aamahfuz.pro@gmail.com>
2023-06-11 21:23:54 +0600

    connected server with client

--- index.js
+++ index.js
@@ -2,11 +2,55 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 // middleware
 app.use(cors());
 app.use(express.json());
 
+//
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f6umtwj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //
+    // classes
+    const classCollection = client.db("onlineDb").collection("classes");
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+    // instructors
+    const instructorsCollection = client
+      .db("onlineDb")
+      .collection("instructors");
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+//
 app.get("/", (req, res) => {
   res.send("online school connected");
 });
