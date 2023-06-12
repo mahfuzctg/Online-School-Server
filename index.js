6cce2b5cfba19558d63005ebe77c119b41cafe64
Mahfuz <aamahfuz.pro@gmail.com>
2023-06-13 02:24:15 +0600

    my class deted done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 //
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f6umtwj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,6 +47,13 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await cartsCollection.insertOne(item);
+      res.send(result);
+    });
+    // carts delete
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     });
     // instructors
